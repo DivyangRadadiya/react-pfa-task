82206992013a673487eb5a53b4a9af6a344a2520
BeastMaxbe <startingplayers@gmail.com>
2023-08-31 19:16:12 +0530

    git init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import DataList from "./component/DataList";
+
+function App() {
+  return (
+    <div className="App">
+      <DataList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/DataList.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { List, ListItem, ListItemText, CircularProgress } from '@material-ui/core';
+
+const API_URL = 'https://hn.algolia.com/api/v1/search_by_date?tags=story&page=';
+
+const DataList = () => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  const observerRef = useRef(null);
+
+  useEffect(() => {
+    fetchNewsData(page);
+    const interval = setInterval(() => {
+      fetchNewsData(page + 1);
+      setPage(prevPage => prevPage + 1);
+    }, 10000);
+    return () => clearInterval(interval);
+  }, [page]);
+
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver(handleObserver, observerOptions);
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+    
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
+      }
+    };
+  }, []);
+
+  const handleObserver = (entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+
+  const fetchNewsData = async (pageNumber) => {
+    try {
+      setLoading(true);
+      const response = await axios.get(API_URL + pageNumber);
+      const newData = response?.data?.hits?.map(item => ({
+        title: item.title,
+        author: item.author,
+        created_at: new Date(item.created_at).toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+        }),
+      }));
+      setData(prevData => [...prevData, ...newData]);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <List>
+        {data?.map((item, index) => (
+          <ListItem key={index}>
+            <ListItemText
+              primary={item.title}
+              secondary={`Author: ${item.author}, Created at: ${item.created_at}`}
+            />
+          </ListItem>
+        ))}
+      </List>
+      {loading && <CircularProgress />}
+      <div ref={observerRef} style={{ height: '20px' }} />
+    </div>
+  );
+};
+
+export default DataList;
+
+
